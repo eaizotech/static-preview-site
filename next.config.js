875ba02b1c257d0051cd7cc78e8e4b63d28b74ef
MaheIT <maheitdev@gmail.com>
2024-12-08 22:45:40 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const isProd = process.env.NODE_ENV === 'production';
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  assetPrefix: isProd ? '/static-preview-site/' : '',
+  // assetPrefix: isProd ? '/static-preview-site' : '',
+  basePath: `/${github}`,
   trailingSlash: true,
     output: 'export',
     distDir: 'docs'
